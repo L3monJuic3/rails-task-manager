8b77b0bed84702ec03bfb3efe60565d7871e15a6
ethan lane <ethan.lane@outlook.co.nz>
2022-08-23 16:47:39 +0100

    oops my commit is too big

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get ''
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  post   'tasks', to: 'tasks#create'
+  get    'tasks/new', to: 'tasks#new', as: :new_task
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update
+  get    'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch  'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
